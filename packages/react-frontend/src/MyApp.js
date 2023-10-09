62f70b9e4c8422ea5dcd832289232e5a86647cd1
Michael Murray <michael.murray.iv@gmail.com>
2023-10-08 20:00:05 -0700

    added CORS (Cross-Origin Resource Sharing) to backend, and load list of characters from backend to frontend

--- packages/react-frontend/src/MyApp.js
+++ packages/react-frontend/src/MyApp.js
@@ -1,28 +1,40 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import Table from './Table';
 import Form from './Form';
 
 function MyApp() {
   const [characters, setCharacters] = useState([]); 
 
-    function removeOneCharacter (index) {
-	    const updated = characters.filter((character, i) => {
-	        return i !== index
-	    });
-	  setCharacters(updated);
-	}
+  function removeOneCharacter (index) {
+    const updated = characters.filter((character, i) => {
+        return i !== index
+    });
+  setCharacters(updated);
+  }
 
-    function updateList(person) {
-        setCharacters([...characters, person]);
-      }
+  function updateList(person) {
+      setCharacters([...characters, person]);
+  }
+
+  function fetchUsers() {
+    const promise = fetch("http://localhost:8000/users");
+    return promise;
+  } 
+
+  useEffect(() => {
+    fetchUsers()
+      .then((res) => res.json())
+      .then((json) => setCharacters(json["users_list"]))
+      .catch((error) => { console.log(error); });
+  }, [] );
       
-    return (
-        <div className="container">
-            <Table characterData={characters} 
-              removeCharacter={removeOneCharacter} />
-            <Form handleSubmit={updateList} />
-        </div>
-      )
+  return (
+      <div className="container">
+          <Table characterData={characters} 
+            removeCharacter={removeOneCharacter} />
+          <Form handleSubmit={updateList} />
+      </div>
+  )
 }
 
 export default MyApp;
