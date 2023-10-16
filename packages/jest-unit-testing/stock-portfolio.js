dc4854e3e927ff7fa58cc455f1c1da5026ecdf4f
Michael Murray <michael.murray.iv@gmail.com>
2023-10-15 17:41:06 -0700

    added get and add functions

--- packages/jest-unit-testing/stock-portfolio.js
+++ packages/jest-unit-testing/stock-portfolio.js
@@ -2,10 +2,16 @@ export default class Portfolio {
     constructor () {
         this.stocks = [];
     }
+    getStocks() {
+        return this.stocks;
+    }
     isEmpty() {
         return this.stocks.length === 0;
     }
     numTickers() {
         return this.stocks.length;
+    }
+    add(symbol, num) {
+        this.stocks.push([symbol, num]);
     }
 };
--- packages/jest-unit-testing/stock-portfolio.test.js
+++ packages/jest-unit-testing/stock-portfolio.test.js
@@ -17,3 +17,11 @@ test('Testing Portfolio.isEmpty -- success', () => {
 test('Testing Portfolio count -- success', () => {
     expect(p.numTickers()).toBe(0)
 });
+
+test('Testing Portfolio add -- success', () => {
+    const expected = [["MSFT", 30]];
+    p.add("MSFT", 30);
+    
+    expect(p.getStocks()).toEqual(expected);
+    
+});
